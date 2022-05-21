c0b0c442ba66a8d78af3751d771990593ebd7d83
EAGLEARENA3 <santos.geronie@gmail.com>
2022-05-21 11:13:40 +0800

    test again

--- backend/src/config.js
+++ backend/src/config.js
@@ -17,12 +17,11 @@ const layerConfigurations = [
     growEditionSizeTo: 5,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Weapon" },
+      { name: "Body" },
+      { name: "Ankle" },
+      { name: "Belt" },
+      { name: "Chest" },
     ],
   },
 ];
