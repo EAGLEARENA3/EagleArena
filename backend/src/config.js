ae2909afbbbe8ba260f7391157d4013ac1d525eb
EAGLEARENA3 <santos.geronie@gmail.com>
2022-05-27 18:39:51 +0800

    zxc

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,7 +7,7 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Eagle Arena";
+const namePrefix = "Eagle Arena TEST";
 const description = "Eagle arena is a game filled with fantastic battle ready Eagles, where players can collect them as pets";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
@@ -46,10 +46,10 @@ const extraMetadata = {
 // ** REQUIRED **
 const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
 const LIMIT = 2; // Your API key rate limit
-const CHAIN = 'polygon'; // only rinkeby or polygon
+const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'Eagle Arnea';
+const CONTRACT_NAME = 'Eagle Arnea TEST';
 const CONTRACT_SYMBOL = 'EAT';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0x22C8889A3a2D00Fb3A40a0e3a0A336Ba28Ff973a';
